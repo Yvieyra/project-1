2d4fc50efb5c3e14f6735fea89b40a20da3edee3
Kate Steed <sunfiresteed@yahoo.com>
2022-12-08 20:31:53 -0700

    hero name, description, and image display

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ const characterName = document.getElementById('characterName');
 const characterDescription = document.getElementById('characterDescription');
 const searchBtn = document.getElementById('searchBtn');
 
+
 //Call to the Youtube API
 
 // let youtubeURL = "https://www.googleapis.com/youtube/v3/search?maxResults=7&key=" + youtubeApiKey + "&q=marvel&q=" + marvelCharacter;
@@ -23,26 +24,36 @@ function getApi() {
 getApi();
 
 
-//Call to the Marvel API
+//Input search data in marvel URL and display 
+
 const M_PRIV_KEY = '9f9f694ad34c04e74f623d0113c1a65d9fa75cb1';
 const M_PUBLIC_KEY = '4565e4658e4d5bd74420b0139cfdd052';
-
-function getMarvelResponse() {
-                                                                                  
+function searchCharacter() {
   let ts = new Date().getTime();
   let hash = CryptoJS.MD5(ts + M_PRIV_KEY + M_PUBLIC_KEY).toString();
 
-
   fetch(
-    `https://gateway.marvel.com:443/v1/public/characters?&ts=${ts}&apikey=${M_PUBLIC_KEY}&hash=${hash}`
+    `https://gateway.marvel.com:443/v1/public/characters?name=${searchBar.value}&ts=${ts}&apikey=${M_PUBLIC_KEY}&hash=${hash}`
   )
 
     .then((response) => response.json())
-    .then ((data) => console.log(data));
+    .then ((data) => {
+      console.log(data);
+      displayHeroInfo(data)
+    });
+}
 
-};
+function displayHeroInfo(data) {
+  const heroName = data.data.results[0].name
+  const heroDescription = data.data.results[0].description
+  const heroImage = data.data.results[0].thumbnail.path + '.' + data.data.results[0].thumbnail.extension
 
-getMarvelResponse();
+
+  characterName.textContent = heroName;
+  characterDescription.textContent = heroDescription;
+  document.getElementById('Characterimage').src = heroImage;
+}
+
 
 
 //Save Searches into Local Storage
@@ -78,32 +89,8 @@ searchBar.addEventListener('keypress', function(e){
   }
 });
 
-//Input search data in marvel url and display *IN PROGRESS*
 
-function searchCharacter() {
-  let ts = new Date().getTime();
-  let hash = CryptoJS.MD5(ts + M_PRIV_KEY + M_PUBLIC_KEY).toString();
 
-  fetch(
-    `https://gateway.marvel.com:443/v1/public/characters?name=${searchBar.value}&ts=${ts}&apikey=${M_PUBLIC_KEY}&hash=${hash}`
-  )
-
-    .then((response) => response.json())
-    .then ((data) => {
-      console.log(data);
-      displayHeroInfo(data)
-    });
-}
-
-function displayHeroInfo(data) { //ERROR: Uncaught (in promise) TypeError: Cannot read properties of undefined (reading '0')//
- 
-  const heroName = data.results[0].name
-  const heroDescription = data.results[0].description
-
-  characterName.textContent = heroName;
-  characterDescription.textContent = heroDescription;
-
-}
 
 
 
