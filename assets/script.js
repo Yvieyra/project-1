9f1ba3912562c9afb0656a5273ab400e98dd0028
Ying Yang <vieyrayessenia8@gmail.com>
2022-12-08 21:37:47 -0700

    added code to display youtube video characters on webpage

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,34 @@
-const searchBar = document.getElementById("search-bar");
-const searchResults = [];
+
+let searchBar = document.getElementById("searchBar");
 const name = document.getElementById('characterName');
 const description = document.getElementById('characterDescription');
+const searchBtn = document.getElementById('searchBtn')
 
+const youtubeVideo = document.getElementById('youtubeVideo')
 //Call to the Youtube API
 
-// let youtubeURL = "https://www.googleapis.com/youtube/v3/search?maxResults=7&key=" + youtubeApiKey + "&q=marvel&q=" + marvelCharacter;
-youtubeApiKey = "AIzaSyDn_KcYLj85JrrXViRDy3henvgOhRsREdM";
+const kateApiKey = "AIzaSyAEoS8iWmmJmS6P5yIJ7lF2fijwJRn0QQQ";
+const yesseniaApiKey = "AIzaSyDiZT7hR0Bp8rjOlzMHm-ui5l78gOwe1dg";
+const andyApiKey = "AIzaSyDn_KcYLj85JrrXViRDy3henvgOhRsREdM";
+
+// let testyoutubeURL = "https://www.googleapis.com/youtube/v3/search?maxResults=1&key=AIzaSyDiZT7hR0Bp8rjOlzMHm-ui5l78gOwe1dg&q=marvel&q=IronMan";
 
 function getApi() {
-  let testyoutubeURL = "https://www.googleapis.com/youtube/v3/search?maxResults=7&key=AIzaSyDn_KcYLj85JrrXViRDy3henvgOhRsREdM&q=marvel&q=spiderman";
-    fetch(testyoutubeURL)
-        .then(function (response) {
-        return response.json();
-        })
-        .then(function (data) {
-       console.log(data)
-        }
-       )};
-
-getApi();
+  marvelCharacter = localStorage.getItem("searches");
+  console.log(marvelCharacter);
+  let youtubeURL = `https://www.googleapis.com/youtube/v3/search?maxResults=1&key=${yesseniaApiKey}&q=marvel&q=${marvelCharacter}`;
+  fetch(youtubeURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      console.log(data.items[0].id.videoId)
+      let videoId = data.items[0].id.videoId
+      youtubeVideo.src = `https://www.youtube.com/embed/${videoId}?rel=0`
+    }
+    )
+};
 
 
 //Call to the Marvel API
@@ -27,7 +36,7 @@ const M_PRIV_KEY = '9f9f694ad34c04e74f623d0113c1a65d9fa75cb1';
 const M_PUBLIC_KEY = '4565e4658e4d5bd74420b0139cfdd052';
 
 function getMarvelResponse() {
-                                                                                  
+
   let ts = new Date().getTime();
   let hash = CryptoJS.MD5(ts + M_PRIV_KEY + M_PUBLIC_KEY).toString();
 
@@ -37,38 +46,38 @@ function getMarvelResponse() {
     ts: ts,
     apikey: M_PUBLIC_KEY,
     hash: hash,
-    })
+  })
 
-    .done(function(data) {
+    .done(function (data) {
       console.log(data);
     }
-)};
+    )
+};
 
 getMarvelResponse();
 
 //Save Searches into Local Storage
-function saveSearch() {
-  localStorage.setItem("searches", JSON.stringify(searchResults));
+function saveSearch(character) {
+  localStorage.setItem("searches", JSON.stringify(character));
 }
 
 //Searches When Enter Key is Pressed
-searchBar.addEventListener('keypress', function(e){
-  if (e.key === 'Enter'){
-  const newSearch = e.target.value;
-  
-  searchResults.push(newSearch);
+// searchBar.addEventListener('keypress', function(e){
+//   if (e.key === 'Enter'){
+//   const newSearch = e.searchBar.value;
 
-  saveSearch();
-  }
-})
+//   searchResults.push(newSearch);
+
+//   saveSearch();
+//   }
+// })
 
 //Searches When Search Btn is Pressed *IN PROGRESS*
-searchBtn.addEventListener('click', function(e){
-  const newSearch = e.target.value;
-  
-  searchResults.push(newSearch);
-
-  saveSearch();
+searchBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  const newSearch = searchBar.value;
+  saveSearch(newSearch);
+  getApi();
 })
 
 
